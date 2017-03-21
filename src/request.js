866645ae450e9bf8c2c2c99f87631715624b41f0
Suuuuha <suha.edris@gmail.com>
2017-03-21 18:20:09 +0200

    removed unnecessary spaces

--- src/request.js
+++ src/request.js
@@ -5,7 +5,7 @@ var urlApixu = 'http://api.apixu.com/v1/current.json?key=' + keys.apixuKey + '&q
 var urlTFL = 'https://api.tfl.gov.uk/road?app_id=' + keys.tflAppid + '&app_key=' + keys.tflKey;
 var urlNews = 'https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=' + keys.newsKey;
 // send a request to the API url
-request(urlNews, function (error, response, body) {
+request(urlNews, function(error, response, body){
   console.log('error:', error); // log the error if one occurred
   console.log('statusCode:', response && response.statusCode); // log the response status code if a response was received
   console.log('body:', JSON.parse(body)); // log the body of the request.
