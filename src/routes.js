945adad6f9b54376d107426e455a8afbd2b028b1
joudy <hilbertbonamy@gmail.com>
2017-03-21 18:04:51 +0200

    adds more comments

--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,11 @@ var routes = {
 
 //Router function to deal with the routes and pass to appropriate handler
 function router(req, res){
+  //if the url of the request exists as a key within our routes object then give (req, res) as arguments to corresponding handler function
   if (routes[req.url]){
     routes[req.url](req, res);
   }
+  //if not, give it the 404
   else{
     routes[404](req, res);
   }
--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 const http = require('http');
 const handler = require('./handler.js');
 const router = require('./routes.js');
-
+//create a server object
 const server = http.createServer(router);
-
+//options object to pass when we start the server
 var options = {
   port: 4000
 }
-
-function startServer(server, options){
-  server.listen(options.port, function(error){
+// create a start server function that takes a server and an options object which contains the port number, tells the server to start listening at that port
+function startServer(serv, opt){
+  serv.listen(opt.port, function(error){
     if (error){
       console.log(error);
     }
     else{
-      console.log('Server is listening on port ',options.port);
+      console.log('Server is listening on port ',opt.port);
     }
   })
 }
-
+//call the function
 startServer(server, options);
