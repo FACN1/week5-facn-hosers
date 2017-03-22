8112642c30a3a418f97892ea42fdb15e0a76f807
Lawson <lgbeaty@live.ca>
2017-03-22 10:01:30 +0200

    Only log an error in requests when one occurs, and remove keys require from request.js

--- src/request.js
+++ src/request.js
@@ -1,25 +1,34 @@
 const request = require('request');
-const keys = require('./keys'); //API Key's
 const urls = require('./urls'); //API URL's
 
 // send a request to the API url's
 var requests = (function(){
   var weather = request(urls.weatherAPI, function(error, response, body){
-    console.log('error: ', error); // log the error if one occurred
+    if(error){
+      console.log('error: ', error);// log the error if one occurred
+    }
     console.log('statusCode: ', response && response.statusCode); // log the response status code if a response was received
-    console.log('body: ', JSON.parse(body)); // log the body of the request.
+    var json = JSON.parse(body);
+    console.log('body: ', json; // log the body of the request.
+    return json;
   });
 
   var roads = request(urls.roadsAPI, function(error, response, body){
-    console.log('error: ', error); // log the error if one occurred
-    console.log('statusCode: ', response && response.statusCode); // log the response status code if a response was received
-    console.log('body: ', JSON.parse(body)); // log the body of the request.
+    if(error){
+      console.log('error: ', error);// log the error if one occurred
+    }    console.log('statusCode: ', response && response.statusCode); // log the response status code if a response was received
+    var json = JSON.parse(body);
+    console.log('body: ', json; // log the body of the request.
+    return json;
   });
 
   var news = request(urls.newsAPI, function(error, response, body){
-    console.log('error: ', error); // log the error if one occurred
-    console.log('statusCode: ', response && response.statusCode); // log the response status code if a response was received
-    console.log('body: ', JSON.parse(body)); // log the body of the request.
+    if(error){
+      console.log('error: ', error);// log the error if one occurred
+    }    console.log('statusCode: ', response && response.statusCode); // log the response status code if a response was received
+    var json = JSON.parse(body);
+    console.log('body: ', json; // log the body of the request.
+    return json;
   });
 
   return{
