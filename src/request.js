89feb811667531633efa65ab2a1969fae7365935
suha-edris <suha.edris@gmail.com>
2017-03-22 16:40:53 +0200

    Create Async handler to provide three API's to render function

--- src/request.js
+++ src/request.js
@@ -2,38 +2,52 @@ const request = require('request');
 const urls = require('./urls'); //API URL's
 
 // send a request to the API url's
-var weather = request(urls.weatherAPI, function(error, response, body){
+function weatherRequest(callback){
+  request(urls.weatherAPI, function(error, response, body){
   if(error){
     console.log('error: ', error);// log the error if one occurred
   }
-  console.log('statusCode: ', response && response.statusCode); // log the response status code if a response was received
-  var json = JSON.parse(body);
-  // console.log('body: ', json); // log the body of the request.
-  return json;
-});
-
-var roads = request(urls.roadsAPI, function(error, response, body){
+  var weather =  JSON.parse(body);
+  callback(null, weather);
+})
+};
+function roadRequest(callback){
+  request(urls.roadsAPI, function(error, response, body){
   if(error){
     console.log('error: ', error);// log the error if one occurred
   }
-  console.log('Roads statusCode: ', response && response.statusCode); // log the response status code if a response was received
-  var json = JSON.parse(body);
-  // console.log('body: ', json); // log the body of the request.
-  return json;
-});
-
-var news = request(urls.newsAPI, function(error, response, body){
+  var road =  JSON.parse(body);
+  callback(null, road)
+})
+};
+function newsRequest(callback){
+  request(urls.newsAPI, function(error, response, body){
   if(error){
-    console.log('News error: ', error);// log the error if one occurred
+    console.log('error: ', error);// log the error if one occurred
   }
-  console.log('News statusCode: ', response && response.statusCode); // log the response status code if a response was received
-  var json = JSON.parse(body);
-  // console.log('body: ', json); // log the body of the request.
-  return json;
-});
+  var news =  JSON.parse(body);
+  callback(null, news)
+})
+};
+// var roads = request(urls.roadsAPI, function(error, response, body){
+//   if(error){
+//     console.log('error: ', error);// log the error if one occurred
+//   }
+//
+//   var JSONres = JSON.parse(body);
+//   return JSONres
+// });
+//
+// var news = request(urls.newsAPI, function(error, response, body){
+//   if(error){
+//     console.log('News error: ', error);// log the error if one occurred
+//   }
+//   var JSONres = JSON.parse(body);
+//   return JSONres
+// });
 
 module.exports = {
-  weather: weather,
-  roads: roads,
-  weather: weather
+  weatherRequest: weatherRequest,
+  roadRequest: roadRequest,
+  newsRequest: newsRequest,
 }
