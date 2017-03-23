09c081c5682ce84a94d868d3d0eff008e9b82ea6
Matt Davis <matthewpeterdavis@gmail.com>
2017-03-23 09:54:20 +0200

    change render function to be more efficient and add traffic and weather

--- public/assets/render.js
+++ public/assets/render.js
@@ -1,17 +1,14 @@
 var renderModule = (function(){
   function renderFeed(responseObject){
-    var oldFeed = document.getElementById('feedContainer');
 
-    var newFeed = document.createElement('main');
+    var newsContainer = document.getElementById('news-container');
+    var roadContainer = document.getElementById('road-container');
+    var weatherContainer = document.getElementById('weather-container');
 
-    newFeed.className = oldFeed.className;
-    newFeed.id = oldFeed.id;
 
-    var newsSection = document.createElement('section');
-    newsSection.className = 'news-container';
-    var newsH2 = document.createElement('h2')
-    newsH2.innerHTML = 'NEWS'
-    var newsList = document.createElement('ul');
+    var oldUL = document.getElementById('newslist');
+    var newUL = document.createElement('ul');
+    newUL.id = oldUL.id
     var articlesArray = responseObject.news.articles;
     articlesArray.forEach(function(article){
       var liNode = document.createElement('li');
@@ -19,25 +16,45 @@ var renderModule = (function(){
       linkNode.setAttribute('href', article.url);
       linkNode.innerHTML = article.description;
       liNode.appendChild(linkNode);
-      newsList.appendChild(liNode);
+      newUL.appendChild(liNode);
     })
-    newsSection.appendChild(newsH2);
-    newsSection.appendChild(newsList);
-    newFeed.appendChild(newsSection);
+
+    var oldULR = document.getElementById('roadlist');
+    var newULR = document.createElement('ul');
+    newULR.id = oldULR.id
+    var roadArray = responseObject.road;
+    roadArray.forEach(function(road){
+      var liNode = document.createElement('li');
+      liNode.innerHTML = road.displayName +': '+ road.statusSeverity;
+      newULR.appendChild(liNode);
+    })
+
+    var oldULW = document.getElementById('weatherlist');
+    var newULW = document.createElement('ul');
+    newULW.id = oldULW.id
+    var weatherLoc = document.createElement('h3')
+    weatherLoc.innerHTML = responseObject.weather.location.name + ": \n"
+    newULW.appendChild(weatherLoc)
+    var weatherArray = responseObject.weather.current;
+    for(var key in weatherArray){
+      if(!weatherArray.hasOwnProperty(key)) continue;
+      var liNode = document.createElement('li');
+      liNode.innerHTML = key +': '+ weatherArray[key];
+      newULW.appendChild(liNode);
+    }
 
 
-    var roadsSection = document.createElement('section');
-    roadsSection.className = 'traffic-container';
-    var roadsH2 = document.createElement('h2').innerHTML = 'TRAFFIC';
-    var roadsList = document.createElement('ul');
+    newsContainer.replaceChild(newUL,oldUL);
+    roadContainer.replaceChild(newULR,oldULR);
+    weatherContainer.replaceChild(newULW,oldULW)
 
 
-    var weatherSection = document.createElement('section');
-    weatherSection.className = 'weather-container';
-    var weatherH2 = document.createElement('h2').innerHTML = 'WEATHER';
-    var weatherList = document.createElement('ul');
+    // var weatherSection = document.createElement('section');
+    // weatherSection.className = 'weather-container';
+    // var weatherH2 = document.createElement('h2').innerHTML = 'WEATHER';
+    // var weatherList = document.createElement('ul');
 
-    oldFeed.parentNode.replaceChild(newFeed, oldFeed);
+    //oldFeed.parentNode.replaceChild(newFeed, oldFeed);
 
   }
   return {
