0c979104b2cacf2a4d81ac10ce9cf372dfb178d7
Lawson <lgbeaty@live.ca>
2017-03-23 12:46:49 +0200

    fix indenting in handler and request issue #42

--- src/request.js
+++ src/request.js
@@ -4,33 +4,33 @@ const urls = require('./urls'); //API URL's
 // send a request to the API url's
 function weatherRequest(callback){
   request(urls.weatherAPI, function(error, response, body){
-  if(error){
-    console.log('error: ', error);// log the error if one occurred
-    return error
-  }
-  var weather =  JSON.parse(body);
-  callback(null, weather);
-})
+    if(error){
+      console.log('error: ', error);// log the error if one occurred
+      return error
+    }
+    var weather =  JSON.parse(body);
+    callback(null, weather);
+  })
 };
 function roadRequest(callback){
   request(urls.roadsAPI, function(error, response, body){
-  if(error){
-    console.log('error: ', error);// log the error if one occurred
-    return error
-  }
-  var road =  JSON.parse(body);
-  callback(null, road)
-})
+    if(error){
+      console.log('error: ', error);// log the error if one occurred
+      return error
+    }
+    var road =  JSON.parse(body);
+    callback(null, road)
+  })
 };
 function newsRequest(callback){
   request(urls.newsAPI, function(error, response, body){
-  if(error){
-    console.log('error: ', error);// log the error if one occurred
-    return error
-  }
-  var news =  JSON.parse(body);
-  callback(null, news)
-})
+    if(error){
+      console.log('error: ', error);// log the error if one occurred
+      return error
+    }
+    var news =  JSON.parse(body);
+    callback(null, news)
+  })
 };
 
 module.exports = {
