d18aaacb1dedefa410e05f6bcbdf169128c19682
Lawson <lgbeaty@live.ca>
2017-03-23 09:55:22 +0200

    change news api to feed BBC stories

--- src/urls.js
+++ src/urls.js
@@ -2,7 +2,7 @@ const keys = require('./keys');
 
 var weatherAPI = 'http://api.apixu.com/v1/current.json?key=' + keys.weather + '&q=London';
 var roadsAPI = 'https://api.tfl.gov.uk/road?app_id=' + keys.roadsID + '&app_key=' + keys.roads;
-var newsAPI = 'https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=' + keys.news;
+var newsAPI = 'https://newsapi.org/v1/articles?source=bbc-news&sortBy=latest&apiKey=' + keys.news;
 
 module.exports = {
     weatherAPI: weatherAPI,
